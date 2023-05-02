1e9d7a2e2b6b8b3770204ff57b6112c020d4df84
Eenclaver <soshnevand@gmail.com>
2023-05-02 18:45:10 +0300

    Fixed critical errors

--- Binary_search.c
+++ Binary_search.c
@@ -7,55 +7,57 @@
 #include <stdlib.h>
 
 
-/**
- * @brief Function to find a place of value in a array
- * @param [in] array array with sorted nums
- * @param [in] element element that we searching in mass
- * @param [out] place place of the element in array
- */
-int binary_Search(char* array, char element,char len)
+ /**
+  * @brief Function to find a place of value in a array
+  * @param [in] array array with sorted nums
+  * @param [in] element element that we searching in mass
+  * @param [out] place place of the element in array
+  */
+int binary_Search(char array[], char element, char len)
 {
-	int place=0;
-	if(element>array[len/2]){
-		for (int i=len/2; i<len;i++){
-			if(element=array[i]){
-				printf<<"Element on place:"<<i<<endl;
-				place =i;
-				break;
-			}
-		}
-	}
-	if(element<array[len/2]){
-		for (int i=0; i<len/2;i++){
-			if(element=array[i]){
-				printf<<"Element on place:"<<i<<endl;
+	int place = 0;
+	if (element > array[5/2]) {
+		for (int i = len / 2; i < len; i++) {
+			if (element == array[i]) {
+				printf("\n");
+				printf( "Element on place:");
+				printf("\n");
+				printf("%d",i+1);
+				printf("\n");
 				place = i;
 				break;
 			}
 		}
 	}
-	if(element=array[len/2]){
-		place = len/2;
-		printf<<"Element on place:"<<len/2<<endl;
+	if ((int)element < (int)array[len/2]) {
+		for (int i = 0; i < len / 2; i++) {
+			if (element == array[i]) {
+				printf("\n");
+				printf("Element on place:");
+				printf("\n");
+				printf("%d", (i + 1));
+				printf("\n");
+				place = i;
+				break;
+			}
+		}
 	}
-	else{
-		printf<<"Element not in array"<<endl;
+	if ((int)element == (int)array[len / 2]) {
+		place = len / 2;
+		printf("\n");
+		printf( "Element on place:");
+		printf("\n");
+		printf("%d",(len / 2) + 1);
+		printf("\n");
+	}
+	else {
+		printf("\n");
+		printf("Element not in array\n");
 		place = -1;
 	}
 	return place;
 }
 
-/**
- * @brief Generate a random number in the given limits
- * @param lim1 lower limit
- * @param lim2 upper limit
- * @return random number in the given range
- */
-int get_rand(int lim1, int lim2)
-{
-    int r = (int)rand() / RAND_MAX;  // value in [0,1)
-    return (lim2 - lim1) * r + lim1;       // scale to range
-}
 
 /**
  * @brief Test implementation
@@ -63,23 +65,30 @@ int get_rand(int lim1, int lim2)
  */
 void test()
 {
-    srand(10);
-    int NUM_TESTS = 5;
-	char[NUM_TESTS] array;
-	char* arrayPtr=array;
-    for (int i = 0; i < NUM_TESTS; i++)
-    {
-		array[i]=(char)get_rand(0,NUM_TESTS);
-		printf("Test %d.... ", i);
-		binary_Search(array,i,i);
-        printf("passed\n");
-    }
+	int NUM_TESTS = 5;
+	char arr[5] = {0};
+	//char* arrPtr = arr;
+	for (int i = 0; i < 5; i++) {
+		arr[i] = (i + 1);
+
+	}
+	printf("Test 1");
+	binary_Search(arr, 3 , 5);
+	printf("Test 2");
+	binary_Search(arr,  'v', 5);
+	printf("Test 3");
+	binary_Search(arr,  -2, 5);
+	printf("Test 4");
+	binary_Search(arr, 15, 5);
+	printf("Test 4");
+	binary_Search(arr, '0', '5');
+	printf("\npassed\n");
 }
 
 /** Main function */
 int main()
 {
-    test();
+	test();
 
-    return 0;
+	return 0;
 }
